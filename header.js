2530216f7abca8749b4a40d8512999714062fbae
Dmitriy <93647407+1swaN@users.noreply.github.com>
2023-12-02 12:52:57 +0400

    some final changes

--- header.js
+++ header.js
@@ -1,14 +1,15 @@
-var prevScrollpos = window.pageYOffset;
+var prevScrollPos = window.scrollY;
 window.onscroll = function() {
-  const header = document.querySelector('.header')
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  let header = document.querySelector('.header');
+  
+  var currentScrollPos = window.scrollY;
+  if (prevScrollPos > currentScrollPos) {
     header.style.top = "0";
     header.style.boxShadow = ' 0 16px 20px rgba(0,0,0,0.2);'
     header.style.transition = 'all .7s ease-out'
-  
+    
   } else {
     document.querySelector(".header").style.top = "-70px";
   }
-  prevScrollpos = currentScrollPos;
+  prevScrollPos = currentScrollPos;
 }
